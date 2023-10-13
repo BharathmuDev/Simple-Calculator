24e70774171e06fb69e408ab43ec8968c81bb36f
Bharath M U <bharath@talentbridge.co.in>
2023-10-13 03:41:19 -0700

    Added functionalities

---
+++ app.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const inputElement = document.querySelector('.input');
+    const outputElement = document.querySelector('.output');
+    const keys = document.querySelectorAll('.key');
+    let isOpenBracket = true;
+
+    keys.forEach(key => {
+        key.addEventListener('click', function() {
+            const keyValue = this.innerText;
+
+            switch (keyValue) {
+                case 'AC':
+                    inputElement.textContent = '';
+                    outputElement.textContent = '';
+                    break;
+                case '=':
+                    try {
+                        const result = eval(inputElement.textContent);
+                        outputElement.textContent = result;
+                    } catch (error) {
+                        outputElement.textContent = 'Error';
+                    }
+                    break;
+                case '( )':
+                    // Toggle between open and close brackets
+                    if (isOpenBracket) {
+                        inputElement.textContent += '(';
+                    } else {
+                        inputElement.textContent += ')';
+                    }
+                    isOpenBracket = !isOpenBracket; // Toggle isOpenBracket variable
+                    break;
+                default:
+                    inputElement.textContent += keyValue;
+                    break;
+            }
+        });
+    });
+});
